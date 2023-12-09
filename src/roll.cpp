237ac020126016fa030ffbcfcdb16e35428fc7b9
maddelin-mcc <143145154+maddelin-mcc@users.noreply.github.com>
2023-12-09 02:59:06 +0000

    Question 2

--- src/roll.cpp
+++ src/roll.cpp
@@ -1,1 +1,19 @@
-//
+#include"roll.h"
+
+//Public fuinctions:
+void Roll::roll_die()
+{
+    //Rolls die1 and die2, saves rolled values to value(private), and sets rolled to true. 
+    die1.roll();
+    die2.roll();
+
+    value = die1.rolled_value() + die2.rolled_value();
+
+    rolled = true;
+
+}
+
+int Roll::roll_value() const
+{
+    return value;
+}
--- test/craps_test.cpp
+++ test/craps_test.cpp
@@ -1,16 +1,29 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "die.h"
+#include "roll.h"
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
 
-TEST_CASE("Verify roll_die returns a value between 1 and 6")
+TEST_CASE("Verify Die.roll_die() returns a value between 1 and 6")
 {
 	Die a;
 	for( int i = 0; i < 10; i++)
 	{
 		a.roll();
 		REQUIRE((a.rolled_value() < 7 && a.rolled_value() > 0));
+	}
+}
+
+TEST_CASE("Verify Roll.roll_die() stores a value between 2 and 12")
+{
+	Die die1;
+	Die die2; 
+	Roll roll(die1, die2);
+	for( int i = 1; i < 10; i++)
+	{
+		roll.roll_die();
+		REQUIRE( (roll.roll_value() > 1 && roll.roll_value() < 13) );
 	}
 }
