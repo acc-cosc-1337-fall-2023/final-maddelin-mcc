59ed04330ec5b1e4e05e6e6f7d24122cc4c6a692
maddelin-mcc <143145154+maddelin-mcc@users.noreply.github.com>
2023-12-09 01:55:33 +0000

    Question 1

--- src/die.cpp
+++ src/die.cpp
@@ -1,1 +1,14 @@
-//
+#include "die.h"
+#include <random>
+#include <math.h>
+//public functions
+    void Die::roll()
+    {
+        //Randomly assigns a value to roll_value in the range of 1 to 6
+        roll_value =  (rand() % 6) + 1;
+    }
+    int Die::rolled_value()
+    {
+        //Returns roll_value
+        return roll_value;
+    }
